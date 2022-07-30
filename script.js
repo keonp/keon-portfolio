6fb74614da9fd233994ccea625a7af298079dc68
keonp <keonlmp@gmail.com>
2022-07-30 14:15:34 -0400

    more nav styles + added about section

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const app = {};
 
-const hamburgerMenu = document.querySelector('.hamburgerMenu');
 const navBar = document.querySelector('.navBar');
+const hamburgerMenu = document.querySelector('.hamburgerMenu');
+const navBarContainer = document.querySelector('.navBarContainer');
+const mobileNav = document.querySelector('.mobileNav');
 
 
 app.displayMobileMenu = () => {
     hamburgerMenu.addEventListener('click', function() {
-
-        // navBar.classList.toggle('extend');
+        navBar.classList.toggle('extendMenuDrop');
+        navBarContainer.classList.toggle('extendMenuBorder');
+        mobileNav.classList.toggle('extendMenuDisplay');
     })
 }
 
