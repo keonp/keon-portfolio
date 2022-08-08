845c42ca778c90a7f91c254a3da123405d2db6a8
keonp <keonlmp@gmail.com>
2022-08-08 09:56:27 -0400

    actual, final commit :older_man:

--- script.js
+++ script.js
@@ -58,9 +58,40 @@ app.menuRemove = () => {
     menuBar[2].classList.remove('bar3');
 }
 
+// a method to fade in elements onto the page
+// Special thanks to pmCodingTutorials on the breakdown on this function:
+// https://www.youtube.com/watch?v=9W7rKLahq2Q
+app.fadeIn = () => {
+    window.addEventListener('DOMContentLoaded', fade);
+
+    function fade() {
+        const observerOptions = {
+            rootMargin: '0px 0px -250px 0px'
+        }
+
+        const observer = new IntersectionObserver((entries, observer) => {
+            entries.forEach((entry) => {
+                if(entry.isIntersecting) {
+                    entry.target.classList.add('show');
+                    observer.unobserve(entry.target);
+                } else {
+                    return;
+                }
+            })
+        }, observerOptions)
+    
+        const fadedElements = document.querySelectorAll('.fade');
+        fadedElements.forEach((element) => {
+            observer.observe(element);
+        })
+    }
+
+}
+
 app.init = () => {
     app.displayMobileMenu();
     app.closeMobileMenu();
+    app.fadeIn();
 }
 
 app.init();
